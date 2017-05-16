e873a18fdfe59bbd071be131de0255c101e48b7f
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-05-16 16:44:05 +0200

    Update fs/opener/__init__.py

--- fs/opener/__init__.py
+++ fs/opener/__init__.py
@@ -1,15 +1,10 @@
 import pkgutil
-import importlib
-
 __path__ = pkgutil.extend_path(__path__, __name__)
-print(__path__)
 
-from ._registry import registry
-from ._utils import manage_fs, parse
-
-for importer, modname, ispkg in pkgutil.iter_modules(__path__):
-    print("Found submodule", modname, "(is a package:", ispkg, ")")
-    importlib.import_module('.'.join([__name__, modname]), package=__name__)
+from . import _loader
+from ._registry import registry, Registry
+from ._utils import manage_fs, parse, ParseResult
+from ._errors import OpenerError, ParseError, Unsupported
 
 open_fs = registry.open_fs
 open = registry.open
