186ef5af67a56dff1edbac330003b5d9b7faac7b
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-05-16 16:38:19 +0200

    Use pkgutil namespace for fs/__init__.py

--- fs/__init__.py
+++ fs/__init__.py
@@ -1,1 +1,6 @@
-__import__('pkg_resources').declare_namespace(__name__)
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+from ._version import __version__
+from .enums import ResourceType, Seek
+from .opener import open_fs
