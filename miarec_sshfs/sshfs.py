5ffecdf9f943f0cb287646ac32b17c33cd1a3abe
gennadiy <gb@miarec.com>
2024-01-12 11:49:41 -0800

    Fis error in move() when preserve_time=True

--- miarec_sshfs/sshfs.py
+++ miarec_sshfs/sshfs.py
@@ -290,7 +290,7 @@ class SSHFS(FS):
             # preserve times if required
             if preserve_time:
                 self._utime(
-                    _path,
+                    _dst_path,
                     src_info.raw["details"]["modified"],
                     src_info.raw["details"]["accessed"],
                 )
